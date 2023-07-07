548ebf2733c35a4e8321f66ea998934a637145c9
Kyrenia <77701345+Kyreniah@users.noreply.github.com>
2023-07-07 16:37:48 +0200

    Solution to stage 4
    Buy, fill, take

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -4,34 +4,96 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has:");
-        int water = scan.nextInt();
-        System.out.println("Write how many ml of milk the coffee machine has:");
-        int milk = scan.nextInt();
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
-        int coffee = scan.nextInt();
-        System.out.println("Write how many cups of coffee you will need:");
-        int cups = scan.nextInt();
-        int possibleCups = amountOfCoffee(cups, water, milk, coffee);
+        int water = 400;
+        int milk = 540;
+        int coffee = 120;
+        int cups = 9;
+        int dollars = 550;
+        displaySupplies(water, milk, coffee, cups, dollars);
+        int[] supplies = {400,540,120,9,550};
 
-        if (cups == possibleCups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (cups > possibleCups) {
-            System.out.printf("No, I can make only %d cup(s) of coffee", possibleCups);
-        } else {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", possibleCups - cups);
+        System.out.println("Write action (buy, fill, take): ");
+        Scanner scan = new Scanner(System.in);
+        String action = scan.next();
+        switch (action) {
+            case "buy":
+                buy(supplies);
+                break;
+            case "fill":
+                fill(supplies);
+                break;
+            case "take":
+                take(supplies);
+                break;
         }
+        displaySupplies(supplies);
     }
-    public static int amountOfCoffee(int cups, int water, int milk, int coffee) {
-        int possibleCups = water / 200;
-        if (milk / 50 < possibleCups) {
-            possibleCups = milk / 50;
+    public static void displaySupplies(int water, int milk, int coffee, int cups, int dollars){
+        System.out.printf("""
+                The coffee machine has:
+                %d ml of water
+                %d ml of milk
+                %d g of coffee beans
+                %d disposable cups
+                $%d of money
+                """, water, milk, coffee, cups, dollars);
+    }
+    public static void displaySupplies(int[] supplies){
+        System.out.printf("""
+                The coffee machine has:
+                %d ml of water
+                %d ml of milk
+                %d g of coffee beans
+                %d disposable cups
+                $%d of money
+                """, supplies[0], supplies[1], supplies[2], supplies[3], supplies[4]);
+    }
+    public static int[] buy(int[] supplies) {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+        Scanner scan = new Scanner(System.in);
+        int order = scan.nextInt();
+        switch (order) {
+            case 1: //espresso
+                supplies[0] -= 250;
+                supplies[2] -= 16;
+                supplies[3]--;
+                supplies[4] += 4;
+                break;
+            case 2: //latte
+                supplies[0] -= 350;
+                supplies[1] -= 75;
+                supplies[2] -= 20;
+                supplies[3]--;
+                supplies[4] += 7;
+                break;
+            case 3: //cappuccino
+                supplies[0] -= 200;
+                supplies[1] -= 100;
+                supplies[2] -= 12;
+                supplies[3]--;
+                supplies[4] += 6;
+                break;
         }
-        if (coffee / 15 < possibleCups) {
-            possibleCups = coffee / 15;
+        return supplies;
+    }
+    public static int[] fill(int[] supplies) {
+        int[] addedAmounts = new int[4];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Write how many ml of water you want to add:");
+        addedAmounts[0] = scan.nextInt();
+        System.out.println("Write how many ml of milk you want to add: ");
+        addedAmounts[1] = scan.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add: ");
+        addedAmounts[2] = scan.nextInt();
+        System.out.println("Write how many disposable cups you want to add: ");
+        addedAmounts[3] = scan.nextInt();
+        for (int i = 0; i < 4; i++) {
+            supplies[i] += addedAmounts[i];
         }
-        return possibleCups;
+        return supplies;
+    }
+    public static void take(int[] supplies) {
+        supplies[4] = 0;
     }
 }
 
